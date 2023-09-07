9f9b48ac412876db0c9a939731299a862c0da127
Elise <elisepolo18@gmail.com>
2023-09-06 22:40:49 -0400

    Initial Commit

---
+++ Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Elise Polo on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
