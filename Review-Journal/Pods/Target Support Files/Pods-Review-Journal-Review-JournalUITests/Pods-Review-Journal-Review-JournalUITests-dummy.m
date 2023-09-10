1b56b7e13e997e466ebb6e5a3c077f338823ea13
Elise <elisepolo18@gmail.com>
2023-09-10 16:57:25 -0400

    Added Firebase necessary imports

---
+++ Review-Journal/Pods/Target Support Files/Pods-Review-Journal-Review-JournalUITests/Pods-Review-Journal-Review-JournalUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Review_Journal_Review_JournalUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Review_Journal_Review_JournalUITests
+@end
