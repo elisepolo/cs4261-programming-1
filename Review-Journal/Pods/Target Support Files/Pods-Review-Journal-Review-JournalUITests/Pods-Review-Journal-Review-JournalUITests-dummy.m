5bd6ff2fd6fc9803bb914909fff88df0a7912901
Elise <elisepolo18@gmail.com>
2023-09-10 18:22:50 -0400

    Revert "Added Firebase necessary imports"
    This reverts commit 1b56b7e13e997e466ebb6e5a3c077f338823ea13.

--- Review-Journal/Pods/Target Support Files/Pods-Review-Journal-Review-JournalUITests/Pods-Review-Journal-Review-JournalUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Review_Journal_Review_JournalUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Review_Journal_Review_JournalUITests
-@end
--- Review-Journal/Pods/Target Support Files/Pods-Review-JournalTests/Pods-Review-JournalTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Review_JournalTests : NSObject
-@end
-@implementation PodsDummy_Pods_Review_JournalTests
-@end
